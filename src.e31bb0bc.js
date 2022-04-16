642f382a19382ca410d2928a4c37ae331a146ab2
joelvaiju <joelvaiju@gmail.com>
2022-04-17 01:18:10 +0530

    Updates

--- src.e31bb0bc.js
+++ src.e31bb0bc.js
@@ -46775,7 +46775,7 @@ __exportStar(require("./browser-connect"), exports);
 require("error-polyfill");
 
 },{"./key_stores/browser-index":"../node_modules/near-api-js/lib/key_stores/browser-index.js","./common-index":"../node_modules/near-api-js/lib/common-index.js","./browser-connect":"../node_modules/near-api-js/lib/browser-connect.js","error-polyfill":"../node_modules/error-polyfill/index.js"}],"config.js":[function(require,module,exports) {
-const CONTRACT_NAME = 'shijo.testnet';
+const CONTRACT_NAME = "dev-1650114593614-93342473530103" || 'shijo.testnet';
 
 function getConfig(env) {
   switch (env) {
@@ -47175,7 +47175,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "45459" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "45353" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
